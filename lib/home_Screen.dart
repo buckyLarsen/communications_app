995fb50d69cd13b9e5e92ed76787a2f040d5faba
Andruw <76119452+Andruw12@users.noreply.github.com>
2023-11-27 19:44:20 -0500

    Create home_Screen.dart

---
+++ lib/home_Screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/folder1');
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(40, 40),
+                backgroundColor: Colors.transparent,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    width: 30,
+                    height: 30,
+                    child: Icon(
+                      Icons.add,
+                      color: const Color.fromARGB(255, 2, 4, 5),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    'Add Item',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20), // Add spacing
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/folder2');
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(40, 40),
+                backgroundColor: Colors.transparent,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    width: 30,
+                    height: 30,
+                    child: Icon(
+                      Icons.settings,
+                      color: const Color.fromARGB(255, 3, 8, 10),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    'Configure',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
