83f160870d9510fba8a379dbfd22a3b266bd3bc2
Andruw <76119452+Andruw12@users.noreply.github.com>
2023-11-30 04:43:12 -0500

    Update home_Screen.dart

--- lib/home_Screen.dart
+++ lib/home_Screen.dart
@@ -1,8 +1,8 @@
+// home_screen.dart
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,56 +12,36 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/folder1');
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(40, 40),
-                backgroundColor: Colors.transparent,
-              ),
-              child: Column(
+            // First Row of Buttons
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    width: 30,
-                    height: 30,
-                    child: Icon(
-                      Icons.add,
-                      color: const Color.fromARGB(255, 2, 4, 5),
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    'Add Item',
-                    style: TextStyle(color: Colors.black),
-                  ),
+                  Expanded(child: buildButton(context, '/add_item', Icons.add, 'Add Item', iconSize: 40.0)),
+                  const SizedBox(width: 20),
+                  Expanded(child: buildButton(context, '/configure', Icons.settings, 'Configure', iconSize: 40.0)),
+                  const SizedBox(width: 20),
+                  Expanded(child: buildButton(context, '/food', Icons.restaurant, 'Food', iconSize: 40.0)),
+                  const SizedBox(width: 20),
+                  Expanded(child: buildButton(context, '/emotions', Icons.mood, 'Emotions', iconSize: 40.0)),
                 ],
               ),
             ),
-            const SizedBox(height: 20), // Add spacing
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/folder2');
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(40, 40),
-                backgroundColor: Colors.transparent,
-              ),
-              child: Column(
+
+            const SizedBox(height: 20), // Add spacing between rows
+
+            // Second Row of Buttons
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    width: 30,
-                    height: 30,
-                    child: Icon(
-                      Icons.settings,
-                      color: const Color.fromARGB(255, 3, 8, 10),
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    'Configure',
-                    style: TextStyle(color: Colors.black),
-                  ),
+                  Expanded(child: buildButton(context, '/activities', Icons.event, 'Activities', iconSize: 40.0)),
+                  const SizedBox(width: 20),
+                  Expanded(child: buildButton(context, '/locations', Icons.map, 'Locations', iconSize: 40.0)),
+                  const SizedBox(width: 20),
+                  Expanded(child: buildButton(context, '/basic_needs', Icons.health_and_safety, 'Basic Needs', iconSize: 40.0)),
+                  const SizedBox(width: 20),
+                  Expanded(child: buildButton(context, '/time_routines', Icons.schedule, 'Time and Routines', iconSize: 40.0)),
                 ],
               ),
             ),
@@ -70,4 +50,42 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+
+  Widget buildButton(BuildContext context, String route, IconData iconData, String label, {double iconSize = 30.0}) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.pushNamed(context, route);
+      },
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(double.infinity, double.infinity),
+        backgroundColor: Colors.transparent,
+        padding: EdgeInsets.all(16.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          side: BorderSide(color: Colors.black, width: 1.0),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: iconSize,
+            height: iconSize,
+            child: Icon(
+              iconData,
+              size: iconSize,
+              color: const Color.fromARGB(255, 3, 8, 10),
+            ),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            label,
+            style: TextStyle(color: Colors.black),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
 }
+
