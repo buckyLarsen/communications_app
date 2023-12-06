1e4160472a8704f29be55d12a3b31925704c8664
Andruw <76119452+Andruw12@users.noreply.github.com>
2023-12-06 09:52:49 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 // main.dart
+import 'package:display/basic_needs_screen.dart';
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
-import 'basic_needs_screen.dart';
 import 'emotions_screen.dart';
 import 'food_screen.dart';
 import 'activities_screen.dart';
 import 'locations_screen.dart';
 import 'time_routines_screen.dart';
-import 'add_Item_Screen.dart';
+import 'objects_screen.dart';
 import 'configure_Data_Screen.dart';
 
 void main() => runApp(const MyApp());
@@ -22,12 +22,15 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const HomeScreen(),
-        '/basic_needs': (context) => const BasicNeedsScreen(),
+        '/objects': (context) => const ObjectsScreen(),
         '/emotions': (context) => const EmotionsScreen(),
        '/food': (context) => const FoodScreen(),
         '/activities': (context) => const ActivitiesScreen(),
         '/locations': (context) => const LocationsScreen(),
-        '/time_routines': (context) => const TimeRoutinesScreen(),
-        '/add_item': (context) => const AddItemScreen(),
+        '/time_routines': (context) => const TimeAndRoutinesScreen(),
         '/configure' : (context) => const ConfigureDataScreen(),
+        '/basic_needs' : (context) => const BasicNeedsScreen(),
       },
+    );
+  }
+}
